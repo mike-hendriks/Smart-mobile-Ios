934c45ff826eb44747dace5a01ae248d152bc737
Dylano Hartman <dylano.hartman@gmail.com>
2019-03-21 13:56:57 +0100

    Wake up init

---
+++ WakeUp/WakeUp/FirstViewController.swift
@@ -0,0 +1,20 @@
+//
+//  FirstViewController.swift
+//  WakeUp
+//
+//  Created by Dylano on 21/03/2019.
+//  Copyright © 2019 Dylano. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
---
+++ WakeUp/WakeUp/SecondViewController.swift
@@ -0,0 +1,20 @@
+//
+//  SecondViewController.swift
+//  WakeUp
+//
+//  Created by Dylano on 21/03/2019.
+//  Copyright © 2019 Dylano. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
